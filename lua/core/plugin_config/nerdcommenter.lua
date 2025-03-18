1ca2fe64191701aed4dee7788a4c2be10d515832
John Erlandsson <john.erlandsson@amokabel.com>
2025-03-18 22:38:25 +0100

    Added nerdcommenter

---
+++ lua/core/plugin_config/nerdcommenter.lua
@@ -0,0 +1,11 @@
+-- Create default mappings
+vim.g.NERDCreateDefaultMappings = 1
+
+-- Add spaces after comment delimiters by default
+vim.g.NERDSpaceDelims = 1
+
+-- Use compact syntax for prettified multi-line comments
+vim.g.NERDCompactSexyComs = 1
+
+-- Enable trimming of trailing whitespace when uncommenting
+vim.g.NERDTrimTrailingWhitespace = 1
--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -23,6 +23,7 @@ return require('packer').startup(function(use)
   use 'preservim/vimux'
   use 'neoclide/coc.nvim'
   use 'rust-lang/rust.vim'
+  use 'connordeckers/nerdcommenter.nvim'
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
