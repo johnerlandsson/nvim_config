c02e5858e573deb6fb6792b1ac1c23e9739178f6
John Erlandsson <john.erlandsson@amokabel.com>
2025-03-19 08:08:53 +0100

    Enabled local config files

--- init.lua
+++ init.lua
@@ -2,5 +2,8 @@ require("core.keymaps")
 require("core.plugins")
 require("core.plugin_config")
 
+-- Copy to clipboard
 vim.opt.clipboard = "unnamedplus"
 
+vim.o.exrc = true
+vim.o.secure = true  -- Prevents execution of unsafe commands
