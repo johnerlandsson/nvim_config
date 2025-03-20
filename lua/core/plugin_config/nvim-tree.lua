72eb4326dc98e75397259dde574e3308eb2ee306
John Erlandsson <john.erlandsson@amokabel.com>
2025-03-20 10:36:02 +0100

    Disabled window_picker

--- lua/core/plugin_config/nvim-tree.lua
+++ lua/core/plugin_config/nvim-tree.lua
@@ -1,6 +1,15 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+  actions = {
+    open_file = {
+      window_picker = {
+        enable = false
+      }
+    }
+  }
+})
 
 vim.keymap.set('n', '<c-n>', ':NvimTreeFindFileToggle<CR>')
+
