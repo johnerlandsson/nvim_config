e31f6c964784c7642a3eeb0c5398ac88a0096762
John Erlandsson <john.erlandsson@amokabel.com>
2025-03-25 06:40:31 +0100

    Added line numbering

--- init.lua
+++ init.lua
@@ -5,5 +5,10 @@ require("core.plugin_config")
 -- Copy to clipboard
 vim.opt.clipboard = "unnamedplus"
 
+-- Allow local config files
 vim.o.exrc = true
 vim.o.secure = true  -- Prevents execution of unsafe commands
+
+-- Setup relative line numbering
+vim.opt.number = true
+vim.opt.relativenumber = true
