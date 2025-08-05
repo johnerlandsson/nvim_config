f0cebb38788bddc03a9377d65beef71de3f6e9a9
John Erlandsson <john.erlandsson@amokabel.com>
2025-08-05 10:35:06 +0200

    Keybindings for nvim-tree

--- lua/core/plugin_config/nvim-tree.lua
+++ lua/core/plugin_config/nvim-tree.lua
@@ -1,15 +1,23 @@
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+local function my_on_attach(bufnr)
+  local api = require "nvim-tree.api"
 
-require("nvim-tree").setup({
-  actions = {
-    open_file = {
-      window_picker = {
-        enable = false
-      }
-    }
-  }
-})
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
 
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- custom mappings
+  vim.keymap.set('n', 'v',     api.node.open.vertical,                opts('Open: Vertical split'))
+  vim.keymap.set('n', '?',     api.tree.toggle_help,                  opts('Help'))
+end
+
+-- pass to setup along with your other options
+require("nvim-tree").setup {
+  ---
+  on_attach = my_on_attach,
+  ---
+}
+# Toggle finder with Ctrl+n
 vim.keymap.set('n', '<c-n>', ':NvimTreeFindFileToggle<CR>')
-
